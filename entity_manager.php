6ac0e09ae7854f06e877407bcc55dc0ba55e7e0c
Giorgio Riccardi <griccardi@my.bcit.ca>
2017-04-19 07:12:48 -0700

    By-pass product switch (generic) in favor entity switch. Created entity_manager logic. Implemented creation of a PHP class instance with a String, from: http://stackoverflow.com/questions/4578335/creating-php-class-instance-with-a-string

---
+++ entity_manager.php
@@ -0,0 +1,10 @@
+<?php
+interface EntityManager
+{
+	public function createEntity( $newProductName );
+	public function listEntities( $where, $orderby, $start, $max );
+	public function	loadEntity( $id );
+	public function updateEntity( $id, $newName );
+	public function deleteEntity( $id );
+}
+?>
--- product_manager.php
+++ product_manager.php
@@ -1,86 +1,85 @@
 <?php
 // create_product.php
 require_once "bootstrap.php";
+require_once "entity_manager.php";
+
  
 use Doctrine\ORM\Doctrine_Query;
-
-function createProduct($newProductName)
+class ProductManager implements EntityManager
 {
+	public function createEntity($newProductName)
+	{
 
-	$product = new Product();
-	$product->setName($newProductName);
+		$product = new Product();
+		$product -> setName($newProductName);
 
-    global $entityManager;
-	$entityManager->persist($product);
-	$entityManager->flush();
-	return $product->getId();
-}
+		global $entityManager;
+		$entityManager -> persist($product);
+		$entityManager -> flush();
+		return $product -> getId();
+	}
 
-function listProducts($where, $orderby, $start, $max)
-{
-	$dql = "SELECT p FROM Product p"; 
-	
-	if($where!=null)
-		$dql = $dql." WHERE BY ".$where;
+	public function listEntities($where, $orderby, $start, $max){
+		$dql = "SELECT p FROM Product p"; 
 		
-	if($orderby!=null)
-		$dql = $dql." ORDER BY ".$orderby;
+		if($where!=null)
+			$dql = $dql." WHERE BY ".$where;
+			
+		if($orderby!=null)
+			$dql = $dql." ORDER BY ".$orderby;
 
-	global $entityManager;
-	$query = $entityManager->createQuery($dql);
-	if($max==null)
-		$max=20;
-	if($start==null)
-		$start=0;
-	$query->setMaxResults($max);
-	
-	return  $query->getArrayResult();
-}
-
-function loadProduct($id)
-{
-	
-	global $entityManager;
-	$product = $entityManager->find('Product', $id);
-
-	if ($product === null) {
-		echo "Product $id does not exist.\n";
-		exit(1);
+		global $entityManager;
+		$query = $entityManager -> createQuery($dql);
+		if($max==null)
+			$max=20;
+		if($start==null)
+			$start=0;
+		$query -> setMaxResults($max);
+		
+		return  $query -> getArrayResult();
 	}
 
-	return $product->toArray();
-}
+	public function loadEntity($id){
+		
+		global $entityManager;
+		$product = $entityManager -> find('Product', $id);
 
-function updateProduct($id, $newName)
-{
-	
-	global $entityManager;
-	$product = $entityManager->find('Product', $id);
+		if ($product === null) {
+			echo "Product $id does not exist.\n";
+			exit(1);
+		}
 
-	if ($product === null) {
-		echo "Product $id does not exist.\n";
-		exit(1);
+		return $product -> toArray();
 	}
 
-	$product->setName($newName);
+	public function updateEntity($id, $newName){
+		
+		global $entityManager;
+		$product = $entityManager -> find('Product', $id);
 
-	$entityManager->flush();
-}
+		if ($product === null) {
+			echo "Product $id does not exist.\n";
+			exit(1);
+		}
 
-function deleteProduct($id)
-{
-	
-	global $entityManager;
-	$product = $entityManager->find('Product', $id);
+		$product -> setName($newName);
 
-	if ($product === null) {
-		echo "Product $id does not exist.\n";
-		exit(1);
+		$entityManager -> flush();
 	}
-	$product = $entityManager->remove($product);
-	$entityManager->flush();
-}
 
+	public function deleteEntity($id){
+		
+		global $entityManager;
+		$product = $entityManager -> find('Product', $id);
+
+		if ($product === null) {
+			echo "Product $id does not exist.\n";
+			exit(1);
+		}
+		$product = $entityManager -> remove($product);
+		$entityManager -> flush();
+	}
+}
 
 
 
