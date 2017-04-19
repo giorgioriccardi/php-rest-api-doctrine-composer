0236232d537cbd852657cc2a1d8ffc62eab9be06
Giorgio Riccardi <griccardi@my.bcit.ca>
2017-04-19 07:12:48 -0700

    By-pass product switch (generic) in favor entity switch. Created entity_manager logic. Implemented creation of a PHP class instance with a String, from: http://stackoverflow.com/questions/4578335/creating-php-class-instance-with-a-string

--- api.php
+++ api.php
@@ -10,37 +10,43 @@ $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $table = $request[0];
 
+switch ($table) 
+{
+	case 'product':
+		$EntityManager = new ProductManager();
+	break;
+}
+
 if(count($request)>1)
 	$entityId = $request[1];
 else
 	$entityId = null;
+	
 switch ($method) 
 {
   case 'GET':
-	if($entityId==null)
-	{
-		$products = listProducts(null, null, null, null); 
-		echo json_encode($products);
+	if($entityId==null){
+		$entities = $EntityManager->listEntities(null, null, null, null); 
+		echo json_encode($entities);
 	}
-	else
-	{
-		$product = loadProduct($entityId);
-		echo json_encode($product);
+	else{
+		$entity = $EntityManager->loadEntity($entityId);
+		echo json_encode($entity);
 	}
 	break;
   case 'PUT':
 	$putdata = file_get_contents("php://input");
 	$request = json_decode($putdata);
-	updateProduct($request->id, $request->name);
+	$EntityManager->updateEntity($request->id, $request->name);
 	break;
   case 'POST':
 	$postdata = file_get_contents("php://input");
 	$request = json_decode($postdata);
-	$id = createProduct($request->name);
+	$id = $EntityManager->createEntity($request->name);
 	echo $id;
 	break;
   case 'DELETE':
-	deleteProduct($entityId);
+	$EntityManager->deleteEntity($entityId);
 	break;
 }
  
